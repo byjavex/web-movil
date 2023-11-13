4daaf67fca5cca8f982ec7f41e29ea16cd943215
javex <byjavex@gmail.com>
2023-11-13 10:09:22 +0100

    a

--- src/routes/Envioformulario.svelte
+++ src/routes/Envioformulario.svelte
@@ -12,8 +12,14 @@
 -->
 
 <script>
+
+
+    let formData = {};
+
     // Función para manejar el clic en el botón.
     function enviarFormulario() {
+
+
         // Muestra una alerta indicando que el formulario ha sido enviado.
         alert("Formulario enviado");
 
@@ -32,7 +38,7 @@
         font-size: 16px;
         background-color: #5bcc26; /* Color de fondo */
         color: #000000; /* Color de texto */
-        border: black; /* Sin borde */
+        border: solid   black; /* Sin borde */
         border-radius: 30px; /* Bordes redondeados */
         transition: background-color 0.3s ease; /* Transición para suavizar el cambio de color de fondo */
         display: block; /* Hace que el botón sea un bloque para poder aplicar márgenes automáticos */
