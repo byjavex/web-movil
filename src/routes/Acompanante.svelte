055dd8010e29efa68b0a50f719fdd6f29a603649
Teodoro <teo.sibon98@gmail.com>
2023-12-04 16:22:47 +0100

    Añadido el componente Acompañante y el formulario de acompañante además de el json de acompañante.

--- src/routes/Acompanante.svelte
+++ src/routes/Acompanante.svelte
@@ -1,9 +1,38 @@
+<!--
 <script>
     import Inputs from "./Inputs.svelte";
     let listaAcompanantes = [];
     for (let i = 0; i < document.getElementById('acompanante').value; i++) {
         listaAcompanantes.push(i);
     }
+</script>
+
+{#each listaAcompanantes as persona (persona)}
+    <Inputs name={"Nombre" + persona} id={"Nombre" + persona} type="text" />
+    <Inputs name={"Apellidos" + persona} id={"Apellidos" + persona} type="text" />
+    <Inputs name={"DNI" + persona} id={"DNI" + persona} type="text" />
+    <Inputs name={"Tipo_Documento" + persona} id={"Tipo_Documento" + persona} type="text" />
+    <Inputs name={"Sexo" + persona} id={"Sexo" + persona} type="text" />
+
+
+    <label for={"fecha_Entrada" + persona}>Fecha entrada <Inputs name={"fecha Entrada" + persona} id={"fecha_Entrada" + persona} type="date" /></label>
+    <label for={"fecha_Nacimiento" + persona}>Fecha nacimiento <Inputs name={"fecha Nacimiento" + persona} id={"fecha_Nacimiento" + persona} type="date" requerido={false} /></label>
+    <label for={"fecha_Expedicion" + persona}>Fecha expedición <Inputs name={"fecha Expedicion" + persona} id={"fecha_Expedicion" + persona} type="date" /></label>
+{/each} -->
+
+
+<script>
+    import Inputs from "./Inputs.svelte";
+    import { onMount } from 'svelte';
+
+    let listaAcompanantes = [];
+
+    onMount(() => {
+        // Esta lógica se ejecutará después de que el componente se monte.
+        for (let i = 0; i < document.getElementById('acompanante').value; i++) {
+            listaAcompanantes.push(i);
+        }
+    });
 </script>
 
 {#each listaAcompanantes as persona (persona)}
